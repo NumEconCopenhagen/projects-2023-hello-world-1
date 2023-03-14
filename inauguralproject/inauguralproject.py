2bec3cb23495bac838a80d4e2606efb4a7abc154
Chensenbassen <124698871+Chensenbassen@users.noreply.github.com>
2023-03-14 17:18:55 +0100

    Update inauguralproject.py

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,20 +1,39 @@
-def square(x):
-    """ square numpy array
-    
-    Args:
-    
-        x (ndarray): input array
-        
-    Returns:
-    
-        y (ndarray): output array
-    
-    """
-    
-    y = x**2
-    return y
+class HouseholdSpecializationModelClass:
 
+    def __init__(self):
+        """ setup model """
 
+        # a. create namespaces
+        par = self.par = SimpleNamespace()
+        sol = self.sol = SimpleNamespace()
+
+        # b. preferences
+        par.rho = 2.0
+        par.nu = 0.001
+        par.epsilon = 1.0
+        par.omega = 0.5 
+
+        # c. household production
+        par.alpha = 0.5
+        par.sigma = 1.0
+
+        # d. wages
+        par.wM = 1.0
+        par.wF = 1.0
+        par.wF_vec = np.linspace(0.8,1.2,5)
+
+        # e. targets
+        par.beta0_target = 0.4
+        par.beta1_target = -0.1
+
+        # f. solution
+        sol.LM_vec = np.zeros(par.wF_vec.size)
+        sol.HM_vec = np.zeros(par.wF_vec.size)
+        sol.LF_vec = np.zeros(par.wF_vec.size)
+        sol.HF_vec = np.zeros(par.wF_vec.size)
+
+        sol.beta0 = np.nan
+        sol.beta1 = np.nan
     def calc_utility(self,LM,HM,LF,HF):
         """ calculate utility """
 
@@ -102,3 +121,4 @@ def square(x):
         """ estimate alpha and sigma """
 
         pass
+    
