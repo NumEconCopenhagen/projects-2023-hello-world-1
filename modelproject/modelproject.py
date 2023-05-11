2e650557a62b61c7853916721b616f734e99f1ed
xqc726 <125376679+xqc726@users.noreply.github.com>
2023-05-11 21:39:28 +0200

    Update modelproject.py

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,22 +1,238 @@
+from types import SimpleNamespace
+import time
+import numpy as np
 from scipy import optimize
 
-def solve_ss(alpha, c):
-    """ Example function. Solve for steady state k. 
+class OLGModelClass():
 
-    Args:
-        c (float): costs
-        alpha (float): parameter
+    def __init__(self,do_print=True):
+        """ create the model """
+        if do_print: print('initializing the model:')
+        
+        self.par = SimpleNamespace()
+        self.sim = SimpleNamespace()
 
-    Returns:
-        result (RootResults): the solution represented as a RootResults object.
+        if do_print: print('calling .setup()')
+        self.setup()
 
-    """ 
+        if do_print: print('calling .allocate()')
+        self.allocate()
     
-    # a. Objective function, depends on k (endogenous) and c (exogenous).
-    f = lambda k: k**alpha - c
-    obj = lambda kss: kss - f(kss)
+    def setup(self):
+        """ baseline parameters """
 
-    #. b. call root finder to find kss.
-    result = optimize.root_scalar(obj,bracket=[0.1,100],method='bisect')
-    
-    return result
+        par = self.par
+
+        # a. household
+        par.sigma = 2.0 # CRRA coefficient
+        par.beta = 1/1.40 # discount factor
+
+        # b. firms
+        par.production_function = 'ces'
+        par.alpha = 0.30 # capital weight
+        par.theta = 0.05 # substitution parameter
+        par.delta = 0.50 # depreciation rate
+
+        # c. government
+        par.tau_w = 0.10 # labor income tax
+        par.tau_r = 0.20 # capital income tax
+
+        # d. misc
+        par.K_lag_ini = 1.0 # initial capital stock
+        par.B_lag_ini = 0.0 # initial government debt
+        par.simT = 50 # length of simulation
+
+        # e. population growth and survival probability 
+        par.p = np.full(par.simT, 0.95)  # survival probability
+        par.n = 0.02  # population growth rate
+        # adjust survival probability for period 10-20
+        par.p[10:20] = 0.6
+
+        # f. population
+        par.pop_initial = 1  # initial young population
+        par.pop_young = [par.pop_initial] + [0] * (par.simT - 1)
+        par.pop_old = [par.pop_initial*par.p[0]/(1+par.n)] + [0] * (par.simT - 1)
+
+
+    def allocate(self):
+        """ allocate arrays for simulation """
+        
+        par = self.par
+        sim = self.sim
+
+        # a. list of variables
+        household = ['C1','C2']
+        firm = ['K','Y','K_lag']
+        prices = ['w','rk','rb','r','rt']
+        government = ['G','T','B','balanced_budget','B_lag']
+
+        # b. allocate
+        allvarnames = household + firm + prices + government
+        for varname in allvarnames:
+            sim.__dict__[varname] = np.nan*np.ones(par.simT)
+
+    def update_population(self, t):
+        """ update the young and old population at time t """
+
+        par = self.par
+
+        # a. new young population is old young population increased by growth rate
+        par.pop_young[t] = par.pop_young[t-1] * (1 + par.n)
+
+        # b. new old population is surviving young population
+        par.pop_old[t] = par.pop_young[t-1] * par.p[t-1]
+
+
+    def simulate(self,do_print=True):
+        """ simulate model """
+
+        t0 = time.time()
+
+        par = self.par
+        sim = self.sim
+
+        # a. initial values
+        sim.K_lag[0] = par.K_lag_ini
+        sim.B_lag[0] = par.B_lag_ini
+
+        # b. iterate
+        for t in range(par.simT):
+
+            # i. update population
+            if t > 0:
+                self.update_population(t)
+
+            # ii. simulate before s
+            simulate_before_s(par,sim,t)
+
+            if t == par.simT-1: continue          
+
+            # iii. find bracket to search
+            s_min,s_max = find_s_bracket(par,sim,t)
+
+            # iv. find optimal s
+            obj = lambda s: calc_euler_error(s,par,sim,t=t)
+            result = optimize.root_scalar(obj,bracket=(s_min,s_max),method='bisect')
+            s = result.root
+
+            # v. simulate after s
+            simulate_after_s(par,sim,t,s)
+
+        if do_print: print(f'simulation done in {time.time()-t0:.2f} secs')
+
+
+def find_s_bracket(par,sim,t,maxiter=500,do_print=False):
+    """ find bracket for s to search in """
+
+    # a. maximum bracket
+    s_min = 0.0 + 1e-8 # save almost nothing
+    s_max = 1.0 - 1e-8 # save almost everything
+
+    # b. saving a lot is always possible 
+    value = calc_euler_error(s_max,par,sim,t)
+    sign_max = np.sign(value)
+    if do_print: print(f'euler-error for s = {s_max:12.8f} = {value:12.8f}')
+
+    # c. find bracket      
+    lower = s_min
+    upper = s_max
+
+    it = 0
+    while it < maxiter:
+                
+        # i. midpoint and value
+        s = (lower+upper)/2 # midpoint
+        value = calc_euler_error(s,par,sim,t)
+
+        if do_print: print(f'euler-error for s = {s:12.8f} = {value:12.8f}')
+
+        # ii. check conditions
+        valid = not np.isnan(value)
+        correct_sign = np.sign(value)*sign_max < 0
+        
+        # iii. next step
+        if valid and correct_sign: # found!
+            s_min = s
+            s_max = upper
+            if do_print: 
+                print(f'bracket to search in with opposite signed errors:')
+                print(f'[{s_min:12.8f}-{s_max:12.8f}]')
+            return s_min,s_max
+        elif not valid: # too low s -> increase lower bound
+            lower = s
+        else: # too high s -> increase upper bound
+            upper = s
+
+        # iv. increment
+        it += 1
+
+    raise Exception('cannot find bracket for s')
+
+def calc_euler_error(s,par,sim,t):
+    """ target function for finding s with bisection """
+
+    # a. simulate forward
+    simulate_after_s(par,sim,t,s)
+    simulate_before_s(par,sim,t+1) # next period
+
+    # c. Euler equation
+    LHS = sim.C1[t]**(-par.sigma)
+    RHS = par.p[t+1] * (1+sim.rt[t+1])*par.beta * sim.C2[t+1]**(-par.sigma)
+
+    return LHS-RHS
+
+def simulate_before_s(par,sim,t):
+    """ simulate forward """
+
+    if t > 0:
+        sim.K_lag[t] = sim.K[t-1]
+        sim.B_lag[t] = sim.B[t-1]
+
+    # a. production and factor prices
+    if par.production_function == 'ces':
+
+        # i. production
+        sim.Y[t] = ( par.alpha*sim.K_lag[t]**(-par.theta) + (1-par.alpha)*((1+par.n)**t)**(-par.theta) )**(-1.0/par.theta)
+
+        # ii. factor prices
+        sim.rk[t] = par.alpha*sim.K_lag[t]**(-par.theta-1) * sim.Y[t]**(1.0+par.theta)
+        sim.w[t] = (1-par.alpha)*(1.0)**(-par.theta-1) * sim.Y[t]**(1.0+par.theta)
+
+    elif par.production_function == 'cobb-douglas':
+
+        # i. production
+        sim.Y[t] = sim.K_lag[t]**par.alpha * (1.0)**(1-par.alpha)
+
+        # ii. factor prices
+        sim.rk[t] = par.alpha * sim.K_lag[t]**(par.alpha-1) * (1.0)**(1-par.alpha)
+        sim.w[t] = (1-par.alpha) * sim.K_lag[t]**(par.alpha) * (1.0)**(-par.alpha)
+
+    else:
+
+        raise NotImplementedError('unknown type of production function')
+
+    # b. no-arbitrage and after-tax return
+    sim.r[t] = sim.rk[t]-par.delta # after-depreciation return
+    sim.rb[t] = sim.r[t] # same return on bonds
+    sim.rt[t] = (1-par.tau_r)*sim.r[t] # after-tax return
+
+    # c. consumption
+    sim.C2[t] = (1 + sim.rt[t]) * (sim.K_lag[t] + sim.B_lag[t]) * par.p[t]
+
+    # d. government
+    sim.T[t] = par.tau_r*sim.r[t]*(sim.K_lag[t]+sim.B_lag[t]) + par.tau_w*sim.w[t]
+
+    if sim.balanced_budget[t]:
+        sim.G[t] = sim.T[t] - sim.r[t]*sim.B_lag[t]
+
+    sim.B[t] = (1+sim.r[t])*sim.B_lag[t] - sim.T[t] + sim.G[t]
+
+def simulate_after_s(par,sim,t,s):
+    """ simulate forward """
+
+    # a. consumption of young
+    sim.C1[t] = (1 - par.tau_w) * sim.w[t] * (1.0 + par.n) ** t * (1 - s)
+
+    # b. end-of-period stocks
+    I = sim.Y[t] - sim.C1[t] - sim.C2[t] - sim.G[t]
+    sim.K[t] = (1 - par.delta) * sim.K_lag[t] + I - sim.K_lag[t] * (1 - par.p[t])
